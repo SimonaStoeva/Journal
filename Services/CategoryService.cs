4bda237e7c6a235fb3c6bae4b74f32ad84e21ec3
SimonaStoeva <s22621604@onlineedu.tu-varna.bg>
2025-08-01 16:20:40 +0300

    Functions handling the user's categories

---
+++ Services/CategoryService.cs
@@ -0,0 +1,75 @@
+﻿using Journal.Models;
+using System.Data.SQLite;
+using Journal.Data;
+
+namespace Journal.Services
+{
+    public class CategoryService
+    {
+        public List<Category> GetCategoriesByUser(int userId)
+        {
+            var categories = new List<Category>();
+
+            using (var connection = DatabaseManager.GetConnection())
+            {
+                connection.Open();
+                var command = new SQLiteCommand("SELECT * FROM Category WHERE UserId = @UserId", connection);
+                command.Parameters.AddWithValue("@UserId", userId);
+
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        categories.Add(ReadCategory(reader));
+                    }
+                }
+            }
+
+            return categories;
+        }
+
+        public Category GetCategoryById(int categoryId)
+        {
+            using (var connection = DatabaseManager.GetConnection())
+            {
+                connection.Open();
+                var command = new SQLiteCommand("SELECT * FROM Category WHERE Id = @Id", connection);
+                command.Parameters.AddWithValue("@Id", categoryId);
+
+                using (var reader = command.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        return ReadCategory(reader);
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        public bool AddCategory(Category category)
+        {
+            using (var connection = DatabaseManager.GetConnection())
+            {
+                connection.Open();
+                var command = new SQLiteCommand("INSERT INTO Category (Name, UserId) VALUES (@Name, @UserId)", connection);
+                command.Parameters.AddWithValue("@Name", category.Name);
+                command.Parameters.AddWithValue("@UserId", category.UserId);
+
+                return command.ExecuteNonQuery() > 0;
+            }
+        }
+        
+
+        private Category ReadCategory(SQLiteDataReader reader)
+        {
+            return new Category
+            {
+                Id = Convert.ToInt32(reader["Id"]),
+                Name = reader["Name"].ToString(),
+                UserId = Convert.ToInt32(reader["UserId"])
+            };
+        }
+    }
+}
