702255ab1131643f7fe6abb29a3e620c43c9f7d4
SimonaStoeva <s22621604@onlineedu.tu-varna.bg>
2025-08-12 19:39:11 +0300

    Updated UI and the logic in the code-behind

---
+++ Views/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using Journal.Data;
+using System.Windows;
+
+namespace Journal.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DatabaseManager.InitializeDatabase();
+        }
+
+        private void Show_Home()
+        {
+            
+            HomeGrid.Visibility = Visibility.Visible;
+            MainFrame.Visibility = Visibility.Collapsed;
+        }
+        
+        public void Logout()
+        {
+            MainFrame.Content = null;
+            Show_Home();
+        }
+
+        public void Log_Click(object sender, RoutedEventArgs e)
+        {
+            HomeGrid.Visibility = Visibility.Collapsed; 
+            MainFrame.Visibility = Visibility.Visible;  
+            MainFrame.Navigate(new LoginPage());        
+        }
+
+        public void Reg_Click(object sender, RoutedEventArgs e)
+        {
+            HomeGrid.Visibility = Visibility.Collapsed; 
+            MainFrame.Visibility = Visibility.Visible;  
+            MainFrame.Navigate(new RegistrationPage()); 
+        }
+
+    }
+}
